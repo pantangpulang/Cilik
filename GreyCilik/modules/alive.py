7801bea90fc3cb0e205dbe4a37aa0255a3dc5bfb
pantangpulang <108105325+pantangpulang@users.noreply.github.com>
2022-06-24 23:09:01 +0700

    Update alive.py

--- GreyCilik/modules/alive.py
+++ GreyCilik/modules/alive.py
@@ -9,16 +9,16 @@ from GreyCilik.events import register
 from GreyCilik import telethn as tbot
 from GreyCilik import BOT_NAME, BOT_USERNAME as bu
 
-PHOTO = "https://telegra.ph/file/760e7c0afaf6ba3df8ce7.jpg"
+PHOTO = "https://telegra.ph/file/49d28b1cd24e029b2574c.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
   GREY = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm {BOT_NAME}.** \n\n"
   GREY += "✪ **I'm Working Properly** \n\n"
-  GREY += f"✪ **My Master : [Grey](https://t.me/greyyvbss)** \n\n"
+  GREY += f"✪ **My Master : [Muk](https://t.me/muk_ajaa)** \n\n"
   GREY += f"✪ **Library Version :** `{telever}` \n\n"
   GREY += f"✪ **Telethon Version :** `{tlhver}` \n\n"
   GREY += f"✪ **Pyrogram Version :** `{pyrover}` \n\n"
   GREY += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ​", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/CilikSupport")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ​", f"https://t.me/{bu}?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/virtualeartt")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=GREY,  buttons=BUTTON)
