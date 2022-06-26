75c36d703d051993c0083dc75788d1ac0d2f98eb
pantangpulang <108105325+pantangpulang@users.noreply.github.com>
2022-06-26 13:37:08 +0700

    Update tagall.py

--- GreyCilik/modules/tagall.py
+++ GreyCilik/modules/tagall.py
@@ -6,7 +6,7 @@ from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin
 from telethon.tl.types import ChannelParticipantCreator
 
-from Cilik import telethn as client
+from GreyCilik import telethn as client
 
 spam_chats = []
 
